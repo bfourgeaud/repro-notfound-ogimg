2c4b6ebd3022adec94ddc8fccce66d735df78e9e
bfourgeaud <benjamin.fourgeaud@gmail.com>
2023-07-04 12:43:09 +0200

    style changes

--- src/app/[subdomain]/page.tsx
+++ src/app/[subdomain]/page.tsx
@@ -1,11 +1,12 @@
-export default function Home() {
+export default function Home({ params: { subdomain } }: { params: { subdomain: string } }) {
   return (
-    <main className="grid gap-6 text-center">
-      <h1>Opengraph-Image not working properly with middleware.</h1>
-      <div>
-        <p>Check head element for meta property og:image</p>
-        <p>The path is not routed though middleware, and therefor not OK</p>
-      </div>
+    <main className="grid gap-6 text-center my-24">
+      <h1 className="text-2xl font-semibold">Opengraph-Image not working properly with middleware.</h1>
+      <blockquote>
+        <p>Check head element for meta property <span className="font-medium">og:image</span></p>
+        <p>The path generated is invalid, and therefore does not present any ogImage to social websites</p>
+        <p>invalid URL : {`http://localhost:3000/${subdomain}/opengraph-image?[id]`}</p>
+      </blockquote>
     </main>
   )
 }
--- src/app/[subdomain]/products/page.tsx
+++ src/app/[subdomain]/products/page.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 
-export default function ProductIndexPage() {
+const linkClass =  "p-2 border border-green-200 cursor-pointer hover:bg-green-100"
+
+export default function ProductIndexPage({ params: { subdomain } }: { params: { subdomain: string } }) {
   return (
-    <main className="">
-      <h1>Click on products</h1>
-      <ul>
-        <li><Link href="/products/1">Exists</Link></li>
-        <li><Link href="/products/2">Does not exist</Link></li>
+    <main className="grid gap-6 my-24 text-center">
+      <h1 className="text-2xl font-semibold">Products for subdomain {subdomain}</h1>
+      <ul className="grid gap-8">
+        <li><Link className={linkClass} href="/products/1">Found</Link></li>
+        <li><Link className={linkClass} href="/products/2">Not Found</Link></li>
       </ul>
     </main>
   )
